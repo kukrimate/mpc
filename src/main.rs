d79fac14277cff2fc9686e8d2d123ec20d316c8a
Mate Kukri <km@mkukri.xyz>
2022-10-07 21:59:37 +0100

    Implement current reference grammar in lalrpop

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,22 @@
+mod ast;
+
+use clap::{Arg,App};
+use std::fs;
 use lalrpop_util::lalrpop_mod;
 
 lalrpop_mod!(parse);
 
 fn main() {
-    assert!(parse::ModuleParser::new().parse("test").is_ok());
+  let args = App::new("bootstrap")
+    .version("0.1.0")
+    .author("Mate Kukri <km@mkukri.xyz>")
+    .about("Boostrap compiler")
+    .arg(Arg::with_name("INPUT")
+      .help("Input file")
+      .required(true)
+      .index(1))
+    .get_matches();
+
+  let input = fs::read_to_string(args.value_of("INPUT").unwrap()).unwrap();
+  println!("{:?}", parse::ModuleParser::new().parse(&input));
 }
