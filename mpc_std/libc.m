72e6e94f988848839dfb9ab3c0afd31e848d7e22
Mate Kukri <kukri.mate@gmail.com>
2023-01-12 14:23:48 +0000

    Improve libc bindings, add type generic {zero,copy}_bytes functions

--- mpc_std/libc.m
+++ mpc_std/libc.m
@@ -3,30 +3,104 @@
  * SPDX-License-Identifier: GPL-2.0-only
  */
 
+// C primitive types (can differ between platform/ABIs)
+struct Void()
+type Char = Int8
+type Uchar = Uint8
+type Short = Int16
+type Ushort = Uint16
+type Int = Int32
+type Uint = Uint32
+type Long = Int64
+type Ulong = Uint64
+type Llong = Int64
+type Ullong = Uint64
+
+// stdio.h
+struct FILE()
+
 extern {
-  function atoi(str: *Int8) -> Int32
+  function printf(fmt: *Char, ...) -> Int
+  function fprintf(stream: *mut FILE, fmt: *Char, ...) -> Int
+  function puts(str: *Char) -> Int
 
-  // stdlib.h
-  function abort()
-  function rand() -> Int32
-  function srand(seed: Uint32)
-
-  // string.h
-  function memcpy(dest: *Uint8, src: *Uint8, len: Uintn) -> *Uint8
-  function memset(dest: *Uint8, c: Int32, len: Uintn) -> *Uint8
-  function memmove(dest: *Uint8, src: *Uint8, len: Uintn) -> *Uint8
-
-  // errno.h
-  data errno: Int32
-
-  // stdio.h
-  data stdin: *mut libc::FILE
-  data stdout: *mut libc::FILE
-  data stderr: *mut libc::FILE
-
-  function printf(fmt: *Int8, ...) -> Int32
-  function fprintf(stream: *mut libc::FILE, fmt: *Int8, ...) -> Int32
-  function puts(str: *Uint8) -> Int32
+  data stdin: *mut FILE
+  data stdout: *mut FILE
+  data stderr: *mut FILE
 }
 
-struct FILE()
+// stdlib.h
+extern {
+  function atof(nptr: *Char) -> Double
+  function atoi(nptr: *Char) -> Int
+  function atol(nptr: *Char) -> Long
+  function atoll(nptr: *Char) -> Llong
+
+  function strtod(nptr: *Char, endptr: *mut *mut Char) -> Double
+  function strtof(nptr: *Char, endptr: *mut *mut Char) -> Float
+  function strtol(nptr: *Char, endptr: *mut *mut Char, base: Int) -> Long
+  function strtoll(nptr: *Char, endptr: *mut *mut Char, base: Int) -> Llong
+  function strtoul(nptr: *Char, endptr: *mut *mut Char, base: Int) -> Ulong
+  function strtoull(nptr: *Char, endptr: *mut *mut Char, base: Int) -> Ullong
+
+  function rand() -> Int
+  function srand(seed: Uint)
+
+  function calloc(nmemb: Uintn, size: Uintn) -> *mut Void
+  function free(ptr: *mut Void)
+  function malloc(size: Uintn) -> *mut Void
+  function realloc(old: *mut Void, size: Uintn) -> *mut Void
+
+  function abort()
+  function atexit(func: Function()) -> Int
+  function exit(status: Int)
+  function _Exit(status: Int)
+
+  function getenv(name: *Char) -> *mut Char
+
+  function system(string: *Char) -> Int
+
+  function bsearch(key: *Void, base: *Void,
+                    nmemb: Uintn, size: Uintn,
+                    compar: Function(a: *Void, b: *Void)) -> *mut Void
+
+  function qsort(base: *mut Void,
+                  nmemb: Uintn, size: Uintn,
+                  compar: Function(a: *Void, b: *Void))
+
+  function abs(j: Int) -> Int
+  function labs(j: Long) -> Long
+  function llabs(j: Llong) -> Llong
+}
+
+// string.h
+extern {
+  function memcpy(dest: *mut Void, src: *Void, len: Uintn) -> *mut Void
+  function memmove(dest: *mut Void, src: *Void, len: Uintn) -> *mut Void
+  function strcpy(s1: *mut Char, s2: *Char) -> *mut Char
+  function strncpy(s1: *mut Char, s2: *Char, n: Uintn) -> *mut Char
+  function strcat(s1: *mut Char, s2: *Char) -> *mut Char
+  function strncat(s1: *mut Char, s2: *Char, n: Uintn) -> *mut Char
+  function memcmp(s1: *Void, s2: *Void, n: Uintn) -> Int
+  function strcmp(s1: *Char, s2: *Char) -> Int
+  function strcoll(s1: *Char, s2: *Char) -> Int
+  function strncmp(s1: *Char, s2: *Char, n: Uintn) -> Int
+  function strxfrm(s1: *Char, s2: *Char, n: Uintn) -> Uintn
+  function memchr(s: *Void, c: Int, n: Uintn) -> *mut Void
+  function strchr(s: *Char, c: Int) -> *mut Char
+  function strcspn(s1: *Char, s2: *Char) -> Uintn
+  function strpbrk(s1: *Char, s2: *Char) -> *mut Char
+  function strrchr(s: *Char, c: Int) -> *mut Char
+  function strspn(s1: *Char, s2: *Char) -> Uintn
+  function strstr(s1: *Char, s2: *Char) -> *mut Char
+  function strtok(s1: *mut Char, s2: *Char) -> *mut Char
+  function memset(s: *mut Void, c: Int, n: Uintn) -> *mut Void
+  function strerror(errnum: Int) -> *mut Char
+  function strlen(s: *Char) -> Uintn
+}
+
+// errno.h
+extern {
+  data errno: Int32
+}
+
---
+++ mpc_std/std.m
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2022-2023, Mate Kukri
+ * SPDX-License-Identifier: GPL-2.0-only
+ */
+
+import libc
+
+function zero_bytes<T>(ptr: *mut T, n: Uintn) {
+  libc::memset(ptr as <*mut libc::Void>, 0, n);
+}
+
+function copy_bytes<T>(to: *mut T, from: *T, n: Uintn) {
+  libc::memmove(to as <*mut libc::Void>, from as <*libc::Void>, n);
+}
