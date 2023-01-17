0a86300d6970aa14a0c68251217f0c468dbced96
Mate Kukri <km@mkukri.xyz>
2023-01-17 10:35:40 +0000

    Remove `PadAdapter` struct from util.rs

--- mpc/src/util.rs
+++ mpc/src/util.rs
@@ -8,46 +8,6 @@ use std::error::Error;
 use std::fmt;
 use std::os::raw::c_char;
 
-/// Same thing std uses for pretty printing
-/// This should really be an exposed API :(
-
-pub struct PadAdapter<'buf> {
-  buf: &'buf mut (dyn fmt::Write + 'buf),
-  state: PadAdapterState,
-}
-
-struct PadAdapterState {
-  on_newline: bool,
-}
-
-impl Default for PadAdapterState {
-  fn default() -> Self {
-    PadAdapterState { on_newline: true }
-  }
-}
-
-impl<'buf> PadAdapter<'buf> {
-  pub fn wrap(fmt: &'buf mut fmt::Formatter<'_>) -> Self {
-    PadAdapter {
-      buf: fmt,
-      state: Default::default(),
-    }
-  }
-}
-
-impl fmt::Write for PadAdapter<'_> {
-  fn write_str(&mut self, s: &str) -> fmt::Result {
-    for s in s.split_inclusive('\n') {
-      if self.state.on_newline {
-        self.buf.write_str("  ")?;
-      }
-      self.state.on_newline = s.ends_with('\n');
-      self.buf.write_str(s)?;
-    }
-    Ok(())
-  }
-}
-
 /// Boxed, type-erased error wrapper
 
 pub type MRes<T> = Result<T, Box<dyn Error>>;
@@ -94,25 +54,25 @@ fn to_owned_c(s: &str) -> String {
   s
 }
 
-impl std::fmt::Display for RefStr {
+impl fmt::Display for RefStr {
   fn fmt(&self, fmt: &mut std::fmt::Formatter<'_>) -> Result<(), std::fmt::Error> {
     self.borrow_rs().fmt(fmt)
   }
 }
 
-impl std::fmt::Debug for RefStr {
+impl fmt::Debug for RefStr {
   fn fmt(&self, fmt: &mut std::fmt::Formatter<'_>) -> Result<(), std::fmt::Error> {
     self.borrow_rs().fmt(fmt)
   }
 }
 
-impl std::cmp::PartialEq for RefStr {
+impl PartialEq for RefStr {
   fn eq(&self, rhs: &RefStr) -> bool {
     self.s as *const str == rhs.s as *const str
   }
 }
 
-impl std::cmp::Eq for RefStr {}
+impl Eq for RefStr {}
 
 impl std::hash::Hash for RefStr {
   fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
